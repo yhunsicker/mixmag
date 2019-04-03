aa42dd4f49f9dcb0351661efa409ffe384369757
yuli hunsicker <yhunsicker@gmail.com>
2019-04-03 13:01:51 +0200

    finalized code challenge

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
-
 document.querySelectorAll(".logo").forEach(logo => logo.addEventListener('click', showhide));
 
 function showhide() {
-var quoteselect = document.getElementById("quote-toggle");
+  var quoteselect = document.getElementById("quote-toggle");
   console.log(quoteselect)
   quoteselect.classList.toggle('visible');
 }
 
+document.getElementById('next').addEventListener('click', slide);
+document.getElementById('prev').addEventListener('click', slide);
 
+function slide() {
+  document.querySelectorAll(".slide").forEach(slide => {
+    slide.classList.toggle('hidden');
 
-
+  });
+}
